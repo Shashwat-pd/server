2a65738697acaeb3b8f57ea67d004ca10f2a962b
shashwat-pd <shashwatp07@gmail.com>
2025-11-02 15:41:59 +0545

    Parser improved

--- src/parser.ts
+++ src/parser.ts
@@ -12,21 +12,46 @@ export type Request = {
 };
 
 export async function getRequestLine(s: Socket) {
-  const methods = ["GET", "POST", "DELETE", "PUT", "HEAD", "PATCH"];
+  const methods = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "HEAD",
+    "PATCH",
+    "OPTIONS",
+    "TRACE",
+    "CONNECT",
+  ];
+
   let requestLine: string = "";
   for await (const line of getLinesChannel(s)) {
+    if (line === "") {
+      continue;
+    }
     requestLine = line;
     break;
   }
 
-  console.log(requestLine)
+  console.log(requestLine);
   //couldBeBetter????????
   let reqArray = requestLine.split(" ");
+  if (reqArray.length > 3) {
+    throw new Error(`Invalid Request Line`);
+  }
   const method = reqArray[0];
   const path = reqArray[1];
   const version = reqArray[2];
+  const regex = /^\/[^\s#]*$/;
 
   if (!methods.includes(method)) {
+    throw new Error(`Unsupported HTTP method: ${method}`);
+  }
+  if (version != "HTTP/1.1") {
+    throw new Error(`Invalid HTTP Version`);
+  }
+  if (!path.match(regex)) {
+    throw new Error(`invalid path`);
   }
 
   return { method, path, version } as RequestLine;
